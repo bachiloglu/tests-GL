2b3b3d934a4fdebba99ca769c3204812ec2d8253
Fernando Catalán <fernandoicatalan@gmail.com>
2022-03-08 19:08:04 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
-import * as estilos from 'style.css'
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
 import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
-
+import * as dat from 'dat.gui'
 
 //Loader
 const textureLoader = new THREE.TextureLoader()
